bb27afa7d920301dc13f4b558ba138ac473a58dc
Pekka Heikura <pekkah@gmail.com>
2018-10-01 22:12:55 +0300

    Observables

--- src/graphql.server.apollo-link/src/client.ts
+++ src/graphql.server.apollo-link/src/client.ts
@@ -6,17 +6,81 @@ import {
   Operation
 } from "apollo-link";
 
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  IStreamResult,
+  IStreamSubscriber,
+  ISubscription
+} from "@aspnet/signalr"
+
+import {
+  createSubject,
+} from "light-observable/observable"
+
+import {
+  Observable as InputObservable,
+  SubscriptionObserver as InputObserver
+} from 'light-observable'
+
 export class Client {
-  private application: Observable<FetchResult>;
+  private hub: HubConnection;
 
-  constructor(private url: string) {}
-
-  public request(operation: Operation): Observable<FetchResult> {
-    this.executeOperation(operation);
-    return this.application;
+  constructor(private url: string) {
+    this.hub = new HubConnectionBuilder().withUrl(url).build();
   }
 
-  private executeOperation(operation: Operation): any {
-    throw new Error("Method not implemented.");
+  public request(operation: Operation): Observable<FetchResult> {
+    var stream = this.execute(operation);
+    var subscription = new Subscription(stream);
+    return subscription.getObservable();
+  }
+
+  private execute(operation: Operation): IStreamResult<FetchResult> {
+    return this.hub.stream<FetchResult>("subscribe", operation);
+  }
+}
+
+class Subscription implements IStreamSubscriber<FetchResult> {
+  closed?: boolean;
+
+  next(value: FetchResult): void {
+    this.sink.next(value);
+  }
+
+  error(err: any): void {
+    this.sink.error(err);
+    this.closed = true;
+  }
+
+  complete(): void {
+    this.sink.complete();
+    this.closed = true;
+  }
+
+  private observable: InputObservable<FetchResult>;
+  private sink: InputObserver<FetchResult>;
+  private streamSubscription: ISubscription<FetchResult>
+
+  constructor(private stream: IStreamResult<FetchResult>) {
+    [this.observable, this.sink] = createSubject();
+    this.streamSubscription = stream.subscribe(this);
+  }
+
+
+
+  public getObservable(): Observable<FetchResult> {
+    return new Observable<FetchResult>(subscriber => {
+      var sub = this.observable.subscribe(
+        next => subscriber.next(next),
+        err => subscriber.error(err),
+        () => subscriber.complete()
+      )
+
+      return () => {
+        sub.unsubscribe();
+        this.streamSubscription.dispose();
+      }
+    })
   }
 }
