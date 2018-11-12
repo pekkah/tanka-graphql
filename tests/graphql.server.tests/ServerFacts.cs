f5b2d109250caea71ae861a1d759f887bc6c1996
Pekka Heikura <pekkah@gmail.com>
2018-11-12 17:58:34 +0200

    Add test for multiple queries

--- tests/graphql.server.tests/ServerFacts.cs
+++ tests/graphql.server.tests/ServerFacts.cs
@@ -67,6 +67,44 @@ namespace fugu.graphql.server.tests
         }
 
         [Fact]
+        public async Task Multiple_Queries()
+        {
+            /* Given */
+            var cts = new CancellationTokenSource();
+            var hubConnection = Connect();
+            await hubConnection.StartAsync();
+
+            /* When */
+            var reader1 = await hubConnection.StreamAsChannelAsync<ExecutionResult>("query", new QueryRequest
+            {
+                Query = "{ hello }"
+            }, cancellationToken: cts.Token);
+
+            var reader2 = await hubConnection.StreamAsChannelAsync<ExecutionResult>("query", new QueryRequest
+            {
+                Query = "{ hello }"
+            }, cancellationToken: cts.Token);
+
+            /* Then */
+            var result1 = await reader1.ReadAsync();
+            var result2 = await reader2.ReadAsync();
+
+            Assert.Contains(result1.Data, kv =>
+            {
+                var (key, value) = kv;
+                return key == "hello" && value.ToString() == "world";
+            });
+
+            Assert.Contains(result2.Data, kv =>
+            {
+                var (key, value) = kv;
+                return key == "hello" && value.ToString() == "world";
+            });
+
+            await hubConnection.StopAsync();
+        }
+
+        [Fact]
         public async Task Subscribe()
         {
             /* Given */
