6345861be5c14fc41da688d6e731ec30880ba393
Pekka Heikura <pekkah@gmail.com>
2019-01-30 06:57:23 +0200

    docs: interfaces

--- docs/1-type-system/07-interfaces.md
+++ docs/1-type-system/07-interfaces.md
@@ -1,1 +1,18 @@
+## Interfaces
 
+> [Specification](https://facebook.github.io/graphql/June2018/#sec-Interfaces)
+
+Interfaces are created as instances of `InterfaceType`. 
+
+### Creating interface
+
+Define with non-null scalar field
+
+[{tanka.graphql.tests.type.InterfaceTypeFacts.Define_interface}]
+
+
+### Implementing interface
+
+Object implements interface
+
+[{tanka.graphql.tests.type.InterfaceTypeFacts.Implement_interface}]
---
+++ tests/graphql.tests/type/InterfaceTypeFacts.cs
@@ -0,0 +1,53 @@
+﻿using tanka.graphql.type;
+using Xunit;
+
+namespace tanka.graphql.tests.type
+{
+    public class InterfaceTypeFacts
+    {
+        [Fact]
+        public void Define_interface()
+        {
+            /* Given */
+            /* When */
+            var namedEntity = new InterfaceType(
+                "NamedEntity",
+                new Fields
+                {
+                    {"name", ScalarType.NonNullString}
+                });
+
+
+            /* Then */
+            Assert.Equal("NamedEntity", namedEntity.Name);
+            Assert.Single(namedEntity.Fields, fk => fk.Key == "name"
+                                                    && (NonNull) fk.Value.Type == ScalarType.NonNullString);
+        }
+
+        [Fact]
+        public void Implement_interface()
+        {
+            /* Given */
+            var namedEntity = new InterfaceType(
+                "NamedEntity",
+                new Fields
+                {
+                    {"name", ScalarType.NonNullString}
+                });
+
+            var person = new ObjectType(
+                "Person",
+                new Fields
+                {
+                    {"name", ScalarType.NonNullString}
+                },
+                implements: new[] {namedEntity});
+
+            /* When */
+            var implements = person.Implements(namedEntity);
+
+            /* Then */
+            Assert.True(implements);
+        }
+    }
+}
