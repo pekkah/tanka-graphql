d4d75149917334d09366bcd02d7b78e6e853bbb5
Pekka Heikura <pekkah@gmail.com>
2019-01-31 11:17:06 +0200

    docs: NamedTypeReference

--- docs/1-type-system/14-named-type-reference.md
+++ docs/1-type-system/14-named-type-reference.md
@@ -1,1 +1,12 @@
+## NamedTypeRereference
 
+Supporting cases where the parent `ObjectType` would have a field of its own type would be difficult without providing a way of referencing the type by its name. This reference would be then replaced by the actual type when `Schema` is initialized.
+
+To reference `INamedType` by its name create instance of `NamedTypeReference` giving it the name of the referenced type as parameter.
+
+
+### Creating NamedTypeReference
+
+Create `ObjectType` with field having the same object as an type using the `NamedTypeReference`. Reference is replaced by the actual type when `Schema` is initialized.
+
+[{tanka.graphql.tests.type.NamedTypeReferenceFacts.Circular_type_reference}]
---
+++ tests/graphql.tests/type/NamedTypeReferenceFacts.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using tanka.graphql.type;
+using Xunit;
+
+namespace tanka.graphql.tests.type
+{
+    public class NamedTypeReferenceFacts
+    {
+        [Fact]
+        public async Task Circular_type_reference()
+        {
+            /* Given */
+            var person = new ObjectType(
+                "Person",
+                new Fields
+                {
+                    {"friends", new List(new NamedTypeReference("Person"))}
+                });
+
+            /* When */
+            var schema = new Schema(new ObjectType("Query", new Fields
+            {
+                {"person", person}
+            }));
+
+            await schema.InitializeAsync();
+            var friendsField = schema.GetNamedType<ObjectType>(person.Name)
+                .GetField("friends");
+
+            /* Then */          
+            Assert.IsType<List>(friendsField.Type);
+            Assert.Equal(person, ((List)friendsField.Type).WrappedType);
+        }
+    }
+}
