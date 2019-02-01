f37bd7c2b913906c2106bc58cee085c99050d0a4
Pekka Heikura <pekkah@gmail.com>
2019-02-01 10:28:14 +0200

    docs: input objects

--- docs/1-type-system/10-input-objects.md
+++ docs/1-type-system/10-input-objects.md
@@ -1,1 +1,16 @@
+## Input Objects
 
+> [Specification](https://facebook.github.io/graphql/June2018/#sec-Input-Objects)
+
+Input objects are created as instances of `InputObjectType`. 
+
+
+### Creating input object
+
+[{tanka.graphql.tests.type.InputObjectTypeFacts.Define}]
+
+> When using List or NonNull wrappers make sure that the wrapped type passes `TypeIs.IsInputType`.
+
+### Input Coercion
+
+[{tanka.graphql.tests.type.InputObjectTypeFacts.Input_coercion}]
---
+++ tests/graphql.tests/type/InputObjectTypeFacts.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using tanka.graphql.execution;
+using tanka.graphql.type;
+using Xunit;
+
+namespace tanka.graphql.tests.type
+{
+    public class InputObjectTypeFacts
+    {
+        [Fact]
+        public void Define()
+        {
+            /* Given */
+            /* When */
+            var input = new InputObjectType(
+                "ExampleInputObject",
+                new InputFields
+                {
+                    {"a", ScalarType.Boolean}
+                });
+
+            /* Then */
+            Assert.Single(input.Fields, fk => fk.Key == "a"
+                                              && (ScalarType) fk.Value.Type == ScalarType.Boolean);
+        }
+
+        [Fact]
+        public void Input_coercion()
+        {
+            /* Given */
+            var input = new InputObjectType(
+                "ExampleInputObject",
+                new InputFields
+                {
+                    {"a", ScalarType.String},
+                    {"b", ScalarType.NonNullInt}
+                });
+
+            /* When */
+            var literalValue = new Dictionary<string, object>
+            {
+                ["a"] = "abc",
+                ["b"] = 123L
+            };
+
+            Dictionary<string, object> actual = 
+                (Dictionary<string, object>)Values.CoerceValue(literalValue, input);
+
+            /* Then */
+            foreach (var expectedKv in literalValue)
+            {
+                Assert.True(actual.ContainsKey(expectedKv.Key));
+                Assert.Equal(expectedKv.Value, actual[expectedKv.Key]);
+            }
+        }
+    }
+}
