852f28142e98d9586911b180a1738936ce1cb7c4
Pekka Heikura <pekkah@gmail.com>
2019-03-26 11:34:07 +0200

    wip

---
+++ src/graphql/introspection/IntrospectionParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using tanka.graphql.type;
+
+namespace tanka.graphql.introspection
+{
+    public class IntrospectionParser
+    {
+        public static __Type ParseTo(string introspectionResult)
+        {
+
+        }
+    }
+}
---
+++ tests/graphql.tests/remotes/ParseIntrospectionFacts.cs
@@ -0,0 +1,20 @@
+﻿using tanka.graphql.introspection;
+using Xunit;
+
+namespace tanka.graphql.tests.remotes
+{
+    public class ParseIntrospectionFacts
+    {
+        [Fact]
+        public void Parse_types()
+        {
+            /* Given */
+            var introspection = "";
+
+            /* When */
+            var typeDefs = IntrospectionParser.ParseTo()
+
+            /* Then */
+        }
+    }
+}
