7c471027142e19e1908f58a6f3449df2f81bdc9d
Pekka Heikura <pekkah@gmail.com>
2019-04-04 23:01:59 +0300

    Add some helpers for importing introspected schema from link (#154)

--- src/graphql/links/RemoteLinks.cs
+++ src/graphql/links/RemoteLinks.cs
@@ -4,16 +4,24 @@ using System.Net.Http;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 using GraphQLParser.AST;
+using Microsoft.AspNetCore.SignalR.Client;
+using tanka.graphql.language;
+using tanka.graphql.requests;
 
 namespace tanka.graphql.links
 {
     public static class RemoteLinks
     {
+        /// <summary>
+        ///     Static data link
+        /// </summary>
+        /// <param name="result">Execution result</param>
+        /// <returns></returns>
         public static ExecutionResultLink Static(ExecutionResult result)
         {
             return async (document, variables, cancellationToken) =>
             {
-                var channel = Channel.CreateUnbounded<ExecutionResult>();
+                var channel = Channel.CreateBounded<ExecutionResult>(1);
 
                 await channel.Writer.WriteAsync(result, cancellationToken);
                 channel.Writer.TryComplete();
@@ -22,6 +30,14 @@ namespace tanka.graphql.links
             };
         }
 
+        /// <summary>
+        ///     Http request link using <see cref="HttpLink" />
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="createClient"></param>
+        /// <param name="transformRequest"></param>
+        /// <param name="transformResponse"></param>
+        /// <returns></returns>
         public static ExecutionResultLink Http(
             string url,
             Func<HttpClient> createClient = null,
@@ -31,6 +47,28 @@ namespace tanka.graphql.links
         {
             var link = new HttpLink(url, createClient, transformRequest, transformResponse);
             return link.Execute;
+        }
+
+        /// <summary>
+        ///     Link for tanka server using SignalR
+        /// </summary>
+        /// <param name="connection">Hub connection to the server</param>
+        /// <returns></returns>
+        public static ExecutionResultLink Server(HubConnection connection)
+        {
+            if (connection.State == HubConnectionState.Disconnected)
+                throw new InvalidOperationException("Connection is disconnected. Start it before using.");
+
+            return async (document, variables, cancellationToken) =>
+            {
+                var channel = await connection.StreamQueryAsync(new QueryRequest
+                {
+                    Query = document.ToGraphQL(),
+                    Variables = variables != null ? new Dictionary<string, object>(variables) : null
+                }, cancellationToken);
+
+                return channel;
+            };
         }
     }
 }
