def8a1d9f5da9ee932e5be9f00253e8ecc5cbceb
Pekka Heikura <pekkah@gmail.com>
2019-04-06 14:00:14 +0300

    SignalR connection needs to be created per execution (#157)
    * SignalR connection needs to be created per execution
    * Use version for Cake.Npm
    * Simplify Server link

--- src/graphql/tools/RemoteSchemaTools.cs
+++ src/graphql/tools/RemoteSchemaTools.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using GraphQLParser.AST;
-using Newtonsoft.Json.Linq;
 using tanka.graphql.channels;
 using tanka.graphql.links;
 using tanka.graphql.resolvers;
@@ -29,7 +28,6 @@ namespace tanka.graphql.tools
 
             // add remote resolver for query
             if (builder.TryGetType<ObjectType>("Query", out var queryType))
-            {
                 builder.Connections(connections =>
                 {
                     var fields = connections.VisitFields(queryType);
@@ -40,10 +38,8 @@ namespace tanka.graphql.tools
                         resolver.Run(createResolver(link));
                     }
                 });
-            }
 
             if (builder.TryGetType<ObjectType>("Mutation", out var mutationType))
-            {
                 builder.Connections(connections =>
                 {
                     var fields = connections.VisitFields(mutationType);
@@ -54,41 +50,75 @@ namespace tanka.graphql.tools
                         resolver.Run(createResolver(link));
                     }
                 });
-            }
 
             if (builder.TryGetType<ObjectType>("Subscription", out var subscriptionType))
-            {
                 builder.Connections(connections =>
                 {
                     var fields = connections.VisitFields(subscriptionType);
 
                     foreach (var field in fields)
-                    {
                         if (!connections.TryGetSubscriber(subscriptionType, field.Key, out _))
                         {
                             var subscriber = connections.GetOrAddSubscriber(subscriptionType, field.Key);
                             subscriber.Run(createSubscriber(link));
                         }
-                    }
                 });
-            }
 
             foreach (var objectType in builder.VisitTypes<ObjectType>())
-            {
                 builder.Connections(connections =>
                 {
                     foreach (var field in connections.VisitFields(objectType))
-                    {
-                        if(!connections.TrGetResolver(objectType, field.Key, out _))
+                        if (!connections.TrGetResolver(objectType, field.Key, out _))
                         {
                             var resolver = connections.GetOrAddResolver(objectType, field.Key);
                             resolver.Run(DefaultDictionaryResolver());
                         }
-                    }
                 });
-            }
 
             return builder.Build();
+        }
+
+        public static Resolver DefaultCreateRemoteResolver(ExecutionResultLink link)
+        {
+            return async context =>
+            {
+                var document = CreateDocument(context);
+                var variables = context.ExecutionContext.CoercedVariableValues;
+
+                var reader = await link(document, variables, CancellationToken.None);
+                while (await reader.WaitToReadAsync(CancellationToken.None))
+                    if (reader.TryRead(out var executionResult))
+                    {
+                        if (executionResult.Errors != null && executionResult.Errors.Any())
+                        {
+                            var first = executionResult.Errors.First();
+                            throw new CompleteValueException(
+                                $"{first.Message}",
+                                new[] {context.Selection},
+                                locations: new[] {context.Selection.Location},
+                                path: context.Path,
+                                extensions: new Dictionary<string, object>
+                                {
+                                    ["remoteError"] = new
+                                    {
+                                        data = executionResult.Data,
+                                        errors = executionResult.Errors,
+                                        extensions = executionResult.Extensions
+                                    }
+                                });
+                        }
+
+                        return new PreExecutedResolveResult(executionResult.Data);
+                    }
+
+                //todo
+                return null;
+            };
+
+            GraphQLDocument CreateDocument(ResolverContext context)
+            {
+                return context.ExecutionContext.Document;
+            }
         }
 
         private static Resolver DefaultDictionaryResolver()
@@ -97,30 +127,16 @@ namespace tanka.graphql.tools
             {
                 object value = null;
                 if (context.ObjectValue is IDictionary<string, object> dictionary)
-                {
                     value = dictionary[context.FieldName];
-                }
-                else if(context.ObjectValue is KeyValuePair<string, object> keyValue)
-                {
+                else if (context.ObjectValue is KeyValuePair<string, object> keyValue)
                     value = keyValue.Value;
-                }
                 else if (context.ObjectValue is ExecutionResult er)
-                {
                     if (er.Data.ContainsKey(context.FieldName))
-                    {
                         value = er.Data[context.FieldName];
-                    }
-                }
 
-                if (value is IDictionary<string, object>)
-                {
-                    return ResolveSync.As(value);
-                }
+                if (value is IDictionary<string, object>) return ResolveSync.As(value);
 
-                if (value is IEnumerable enumerable && !(value is string))
-                {
-                    return ResolveSync.As(enumerable);
-                }
+                if (value is IEnumerable enumerable && !(value is string)) return ResolveSync.As(enumerable);
 
                 return ResolveSync.As(value);
             };
@@ -139,63 +155,14 @@ namespace tanka.graphql.tools
                 var _ = Task.Run(async () =>
                 {
                     while (await result.WaitToReadAsync(unsubscribe))
-                    {
                         if (result.TryRead(out var executionResult))
-                        {
                             await stream.WriteAsync(
                                 executionResult);
-                        }
-                    }
 
                     await result.Completion;
                 }, unsubscribe);
 
                 return Resolve.Subscribe(stream, unsubscribe);
-            };
-
-            GraphQLDocument CreateDocument(ResolverContext context)
-            {
-                return context.ExecutionContext.Document;
-            }
-        }
-
-        public static Resolver DefaultCreateRemoteResolver(ExecutionResultLink link)
-        {
-            return async context =>
-            {
-                var document = CreateDocument(context);
-                var variables = context.ExecutionContext.CoercedVariableValues;
-
-                var reader = await link(document, variables, CancellationToken.None);
-                while (await reader.WaitToReadAsync(CancellationToken.None))
-                {
-                    if (reader.TryRead(out var executionResult))
-                    {
-                        if (executionResult.Errors != null && executionResult.Errors.Any())
-                        {
-                            var first = executionResult.Errors.First();
-                            throw new CompleteValueException(
-                                $"{first.Message}",
-                                nodes:new []{ context.Selection},
-                                locations: new []{context.Selection.Location},
-                                path: context.Path,
-                                extensions: new Dictionary<string, object>()
-                                {
-                                    ["remoteError"] = new
-                                    {
-                                        data = executionResult.Data,
-                                        errors = executionResult.Errors,
-                                        extensions = executionResult.Extensions
-                                    } 
-                                });
-                        }
-
-                        return new PreExecutedResolveResult(executionResult.Data);
-                    }
-                }
-
-                //todo
-                return null;
             };
 
             GraphQLDocument CreateDocument(ResolverContext context)
