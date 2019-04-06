06d83ad2a2db2d7b5f583e63b512c3dfdf771f29
Pekka Heikura <pekkah@gmail.com>
2019-04-06 13:01:58 +0300

    fix: Input types not merged if does not exists in both schemas (#156)

--- src/graphql/tools/MergeTool.cs
+++ src/graphql/tools/MergeTool.cs
@@ -1,106 +1,130 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using tanka.graphql.type;
 
 namespace tanka.graphql.tools
 {
     public static class MergeTool
     {
+        [Obsolete("Use MergeTool.Schema")]
         public static ISchema MergeSchemas(
             ISchema left,
             ISchema right)
         {
             var builder = new SchemaBuilder(left);
+            Schema(builder, right);
+            return builder.Build();
+        }
 
+        public static void Schemas(SchemaBuilder target, params ISchema[] schemas)
+        {
+            foreach (var right in schemas) Schema(target, right);
+        }
+
+        public static void Schema(SchemaBuilder target, ISchema right)
+        {
             foreach (var rightType in right.QueryTypes<ComplexType>())
-                if (builder.TryGetType<ComplexType>(rightType.Name, out var leftType))
-                {
-                    var rightTypeFields = right.GetFields(rightType.Name);
+                MergeComplexType(right, target, rightType);
 
-                    foreach (var rightTypeField in rightTypeFields)
+            foreach (var rightType in right.QueryTypes<InputObjectType>()) MergeInputType(right, target, rightType);
+
+            foreach (var rightType in right.QueryTypes<ScalarType>()) MergeScalarType(target, rightType);
+
+            foreach (var directiveType in right.QueryDirectiveTypes())
+            {
+                target.TryGetDirective(directiveType.Name, out var leftDirective);
+
+                if (leftDirective != null)
+                    continue;
+
+                target.IncludeDirective(directiveType);
+            }
+        }
+
+        private static void MergeScalarType(SchemaBuilder builder, ScalarType rightType)
+        {
+            if (!builder.TryGetType<ScalarType>(rightType.Name, out _)) builder.Include(rightType);
+        }
+
+        private static void MergeInputType(ISchema right, SchemaBuilder builder, InputObjectType rightType)
+        {
+            if (builder.TryGetType<InputObjectType>(rightType.Name, out var leftType))
+            {
+                var rightTypeFields = right.GetInputFields(rightType.Name);
+
+                foreach (var rightTypeField in rightTypeFields)
+                    builder.Connections(connect =>
                     {
-                        builder.Connections(connect =>
-                        {
-                            if (connect.TryGetField(leftType, rightTypeField.Key, out _)) 
-                                return;
-                            
-                            connect.IncludeFields(leftType, new[] {rightTypeField});
+                        if (connect.TryGetInputField(leftType, rightTypeField.Key, out _))
+                            return;
 
+                        connect.IncludeInputFields(leftType, new[] {rightTypeField});
+                    });
+            }
+            else
+            {
+                builder
+                    .Include(rightType)
+                    .Connections(connect =>
+                    {
+                        var fields = right.GetInputFields(rightType.Name).ToList();
+                        connect.IncludeInputFields(rightType, fields);
+                    });
+            }
+        }
+
+        private static void MergeComplexType(ISchema right, SchemaBuilder builder, ComplexType rightType)
+        {
+            if (builder.TryGetType<ComplexType>(rightType.Name, out var leftType))
+            {
+                var rightTypeFields = right.GetFields(rightType.Name);
+
+                foreach (var rightTypeField in rightTypeFields)
+                    builder.Connections(connect =>
+                    {
+                        if (connect.TryGetField(leftType, rightTypeField.Key, out _))
+                            return;
+
+                        connect.IncludeFields(leftType, new[] {rightTypeField});
+
+                        var resolver = right.GetResolver(rightType.Name, rightTypeField.Key);
+
+                        if (resolver != null)
+                            connect.GetOrAddResolver(leftType, rightTypeField.Key)
+                                .Run(resolver);
+
+                        var subscriber = right.GetSubscriber(rightType.Name, rightTypeField.Key);
+
+                        if (subscriber != null)
+                            connect.GetOrAddSubscriber(leftType, rightTypeField.Key)
+                                .Run(subscriber);
+                    });
+            }
+            else
+            {
+                builder
+                    .Include(rightType)
+                    .Connections(connect =>
+                    {
+                        var fields = right.GetFields(rightType.Name).ToList();
+                        connect.IncludeFields(rightType, fields);
+
+                        foreach (var rightTypeField in fields)
+                        {
                             var resolver = right.GetResolver(rightType.Name, rightTypeField.Key);
 
                             if (resolver != null)
-                            {
-                                connect.GetOrAddResolver(leftType, rightTypeField.Key)
+                                connect.GetOrAddResolver(rightType, rightTypeField.Key)
                                     .Run(resolver);
-                            }
 
                             var subscriber = right.GetSubscriber(rightType.Name, rightTypeField.Key);
 
                             if (subscriber != null)
-                            {
-                                connect.GetOrAddSubscriber(leftType, rightTypeField.Key)
+                                connect.GetOrAddSubscriber(rightType, rightTypeField.Key)
                                     .Run(subscriber);
-                            }
-                        });
-                    }
-                }
-                else
-                {
-                    builder
-                        .Include(rightType)
-                        .Connections(connect =>
-                        {
-                            var fields = right.GetFields(rightType.Name).ToList();
-                            connect.IncludeFields(rightType, fields);
-
-                            foreach (var rightTypeField in fields)
-                            {
-                                var resolver = right.GetResolver(rightType.Name, rightTypeField.Key);
-
-                                if (resolver != null)
-                                {
-                                    connect.GetOrAddResolver(rightType, rightTypeField.Key)
-                                        .Run(resolver);
-                                }
-
-                                var subscriber = right.GetSubscriber(rightType.Name, rightTypeField.Key);
-
-                                if (subscriber != null)
-                                {
-                                    connect.GetOrAddSubscriber(rightType, rightTypeField.Key);
-                                }
-                            }
-                        });
-                }
-
-
-            foreach (var rightType in right.QueryTypes<InputObjectType>())
-            {
-                if (builder.TryGetType<InputObjectType>(rightType.Name, out var leftType))
-                {
-                    var rightTypeFields = right.GetInputFields(rightType.Name);
-
-                    foreach (var rightTypeField in rightTypeFields)
-                    {
-                        builder.Connections(connect =>
-                        {
-                            if (connect.TryGetInputField(leftType, rightTypeField.Key, out _)) 
-                                return;
-                            
-                            connect.IncludeInputFields(leftType, new[] {rightTypeField});
-                        });
-                    }
-                }
+                        }
+                    });
             }
-
-            foreach (var rightType in right.QueryTypes<ScalarType>())
-            {
-                if (!builder.TryGetType<ScalarType>(rightType.Name, out _))
-                {
-                    builder.Include(rightType);
-                }
-            }
-
-            return builder.Build();
         }
     }
 }
