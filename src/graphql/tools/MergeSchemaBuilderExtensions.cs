bb2faaa9bb3136d5f567e3b10e09fe365c26b685
Pekka Heikura <pekkah@gmail.com>
2019-04-06 12:49:21 +0300

    Include file

---
+++ src/graphql/tools/MergeSchemaBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using tanka.graphql.type;
+
+namespace tanka.graphql.tools
+{
+    public static class MergeSchemaBuilderExtensions
+    {
+        public static SchemaBuilder Merge(this SchemaBuilder builder, params ISchema[] schemas)
+        {
+            MergeTool.Schemas(builder, schemas);
+            return builder;
+        }
+    }
+}
