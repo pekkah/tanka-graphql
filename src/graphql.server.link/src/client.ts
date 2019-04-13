f778fc6b88da6e2984f432e92c183df8cf3b8155
Pekka Heikura <pekkah@gmail.com>
2019-04-13 18:27:35 +0300

    Start/stop connection (#166)
    * Start/stop connection
    * Apollo: Automatically try to reconnect if connection lost
    * Apollo: Move reconnect settings into options

--- src/graphql.server.link/src/client.ts
+++ src/graphql.server.link/src/client.ts
@@ -1,7 +1,7 @@
-import { 
-  FetchResult, 
-  Observable, 
-  Operation 
+import {
+  FetchResult,
+  Observable,
+  Operation
 } from "apollo-link";
 
 import {
@@ -13,25 +13,33 @@ import {
 import { Request } from "./request";
 import { Subscription } from "./subscription";
 
-export class TankaClient {
-  private hub: HubConnection;
-  private started: Promise<void> = undefined;
+export class ClientOptions {
+  connection: IHttpConnectionOptions;
+  reconnectAttempts: number = 10;
+  reconnectInitialWaitMs: number = 1000;
+  reconnectAdditionalWaitMs: number = 500;
+}
 
-  constructor(private url: string, private options?: IHttpConnectionOptions) {
+export class TankaClient {
+  private BACKGROUND_QUEUE_TIMER_MS = 1000;
+  private hub: HubConnection;
+  private connected: boolean = false;
+  private connecting: boolean = false;
+  private buffer: { sub: Subscription, op: Operation }[] = [];
+  private backgroundQueueTimerId: NodeJS.Timeout;
+  private reconnectTimerId: NodeJS.Timeout;
+
+  constructor(private url: string, private options: ClientOptions) {
     const builder = new HubConnectionBuilder();
 
-    if (options) {
-      builder.withUrl(url, options);
+    if (options && options.connection) {
+      builder.withUrl(url, options.connection);
     } else {
       builder.withUrl(url);
     }
 
     this.hub = builder.build();
-    this.started = new Promise<void>(resolve => {
-      this.hub.start().then(() => {
-        resolve();
-      });
-    });
+    this.hub.onclose(() => this.onClosed);
   }
 
   public request(operation: Operation): Observable<FetchResult> {
@@ -60,11 +68,105 @@ export class TankaClient {
   public query(operation: Operation): Subscription {
     const sub = new Subscription();
 
-    this.started.then(() => {
-      const stream = this.hub.stream("query", new Request(operation));
-      sub.subscribe(stream);
-    });
+    if (this.connected) {
+      try {
+        const stream = this.hub.stream("query", new Request(operation));
+        sub.subscribe(stream);
+      }
+      catch (e) {
+        this.onClosed(e);
+        this.queue(sub, operation);
+      }
+    } else {
+      this.start(() => { });
+      this.queue(sub, operation);
+    }
 
     return sub;
+  }
+
+  private queue(subscription: Subscription, operation: Operation) {
+    this.buffer.push({ sub: subscription, op: operation });
+  }
+
+  private processQueue() {
+    while (this.buffer.length > 0) {
+      if (!this.connected)
+        break;
+
+      const { sub, op } = this.buffer.pop();
+      try {
+        const stream = this.hub.stream("query", new Request(op));
+        sub.subscribe(stream);
+      }
+      catch (e) {
+        this.onClosed(e);
+        this.queue(sub, op);
+      }
+    }
+  }
+
+  private reconnect() {
+    let count = 0;
+    this.reconnectTimerId = setInterval(() => {
+      this.start(() => {
+        clearInterval(this.reconnectTimerId);
+        console.log(`Connected`)
+      });
+
+      if (this.connected) {
+        return;
+      }
+
+      count++;
+      console.log(`Connection attempt #${count}`);
+
+      if (count >= this.options.reconnectAttempts) {
+        clearInterval(this.reconnectTimerId);
+        this.onClosed();
+      }
+    }, this.options.reconnectInitialWaitMs + (count * this.options.reconnectAdditionalWaitMs));
+  }
+
+  private start(callback: () => void) {
+    if (this.connected) {
+      callback();
+      return;
+    }
+
+    if (this.connecting) {
+      return;
+    }
+
+    this.connecting = true;
+    return this.hub.start().then(() => {
+      this.connected = true;
+      this.connecting = false;
+      this.backgroundQueueTimerId = setInterval(() => this.processQueue(), this.BACKGROUND_QUEUE_TIMER_MS);
+    })
+      .catch(err => {
+        this.connected = false;
+        this.connecting = false;
+        throw err;
+      })
+  }
+
+  private onClosed(error?: Error) {
+    this.connected = false;
+
+    if (this.backgroundQueueTimerId) {
+      clearInterval(this.backgroundQueueTimerId);
+    }
+
+    if (error) {
+      console.log("Connection closed due to error", error);
+      this.reconnect();
+    }
+    else {
+      console.log("Connection closed.");
+      if (this.reconnectTimerId) {
+        clearInterval(this.reconnectTimerId);
+      }
+    }
   }
 }
---
+++ tests/graphql.tests/tools/MakeRemoteExecutableDigitransitFacts.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using tanka.graphql.introspection;
+using tanka.graphql.language;
+using tanka.graphql.links;
+using tanka.graphql.tests.data;
+using tanka.graphql.tools;
+using tanka.graphql.type;
+using tanka.graphql.type.converters;
+using Xunit;
+
+namespace tanka.graphql.tests.tools
+{
+    public class MakeRemoteExecutableDigitransitFacts
+    {
+        private static string GetDigitransitIntrospection()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceStream = assembly.GetManifestResourceStream("tanka.graphql.tests.digitransit.introspection");
+            using (var reader =
+                new StreamReader(resourceStream ?? throw new InvalidOperationException(), Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        [Fact]
+        public async Task ExecuteRemotely()
+        {
+            /* Given */
+            var builder = new SchemaBuilder()
+                .Scalar("Long", out _, new StringConverter())
+                .Scalar("Lat", out _, new StringConverter())
+                .Scalar("Polyline", out _, new StringConverter())
+                .ImportIntrospectedSchema(GetDigitransitIntrospection());
+
+            /* When */
+            var schema = RemoteSchemaTools.MakeRemoteExecutable(
+                builder,
+                link: RemoteLinks.Http(
+                    url: "https://api.digitransit.fi/routing/v1/routers/next-hsl/index/graphql"
+                    )
+                );
+
+            var result = await Executor.ExecuteAsync(new ExecutionOptions
+            {
+                Schema = schema,
+                Document = Parser.ParseDocument(@"
+                    {
+                        feeds {
+                            feedId
+                        }
+                    }
+                    ")
+            });
+
+            /* Then */
+            result.ShouldMatchJson(@"
+              {
+              ""data"": {
+                ""feeds"": [
+                  {
+                    ""feedId"": ""HSL""
+                  },
+                  {
+                    ""feedId"": ""HSLlautta""
+                  }
+                ]
+              }
+            }
+            ");
+        }
+
+        [Fact]
+        public async Task ExecuteWithStaticDataLink()
+        {
+            /* Given */
+            var builder = new SchemaBuilder()
+                .Scalar("Long", out _, new StringConverter())
+                .Scalar("Lat", out _, new StringConverter())
+                .Scalar("Polyline", out _, new StringConverter())
+                .ImportIntrospectedSchema(GetDigitransitIntrospection());
+
+            /* When */
+            var schema = RemoteSchemaTools.MakeRemoteExecutable(
+                builder,
+                link: RemoteLinks.Static(new ExecutionResult
+                {
+                    Data = new Dictionary<string, object>
+                    {
+                        ["feeds"] = new Dictionary<string, object>
+                        {
+                            ["feedId"] = "123"
+                        }
+                    }
+                }));
+
+            var result = await Executor.ExecuteAsync(new ExecutionOptions
+            {
+                Schema = schema,
+                Document = Parser.ParseDocument(@"
+                    {
+                        feeds {
+                            feedId
+                        }
+                    }
+                    ")
+            });
+
+            /* Then */
+            result.ShouldMatchJson(
+                @"
+                {
+                  ""data"": {
+                    ""feeds"": [
+                      {
+                        ""feedId"": ""123""
+                      }
+                    ]
+                  }
+                }
+            ");
+        }
+
+        [Fact]
+        public async Task RemoteExecute_with_link_exception()
+        {
+            /* Given */
+            var builder = new SchemaBuilder()
+                .Scalar("Long", out _, new StringConverter())
+                .Scalar("Lat", out _, new StringConverter())
+                .Scalar("Polyline", out _, new StringConverter())
+                .ImportIntrospectedSchema(GetDigitransitIntrospection());
+
+            /* When */
+            var schema = RemoteSchemaTools.MakeRemoteExecutable(
+                builder,
+                (document, variables, cancellationToken) =>
+                    throw new InvalidOperationException("error"));
+
+            var result = await Executor.ExecuteAsync(new ExecutionOptions
+            {
+                Schema = schema,
+                Document = Parser.ParseDocument(@"
+                    {
+                        feeds {
+                            feedId
+                        }
+                    }
+                    ")
+            });
+
+            /* Then */
+            result.ShouldMatchJson(@"
+                {
+                  ""data"": {
+                    ""feeds"": null
+                  },
+                  ""errors"": [
+                    {
+                      ""message"": ""error"",
+                      ""extensions"": {
+                        ""code"": ""INVALIDOPERATION""
+                      }
+                    }
+                  ]
+                }
+            ");
+        }
+
+        [Fact]
+        public async Task ExecuteRemotely_with_link_graphql_error()
+        {
+            /* Given */
+            var builder = new SchemaBuilder()
+                .Scalar("Long", out _, new StringConverter())
+                .Scalar("Lat", out _, new StringConverter())
+                .Scalar("Polyline", out _, new StringConverter())
+                .ImportIntrospectedSchema(GetDigitransitIntrospection());
+
+            /* When */
+            var schema = RemoteSchemaTools.MakeRemoteExecutable(
+                builder,
+                async (document, variables, cancellationToken) =>
+                {
+                    var channel = Channel.CreateBounded<ExecutionResult>(1);
+                    var executionResult = new ExecutionResult
+                    {
+                        Errors = new[]
+                        {
+                            new Error("failed to find...")
+                        }
+                    };
+                    await channel.Writer.WriteAsync(executionResult, cancellationToken);
+                    channel.Writer.TryComplete();
+
+                    return channel;
+                });
+
+            var result = await Executor.ExecuteAsync(new ExecutionOptions
+            {
+                Schema = schema,
+                Document = Parser.ParseDocument(@"
+                    {
+                        feeds {
+                            feedId
+                        }
+                    }
+                    ")
+            });
+
+            /* Then */
+            result.ShouldMatchJson(@"
+                {
+                  ""data"": {
+                    ""feeds"": null
+                  },
+                  ""errors"": [
+                    {
+                      ""message"": ""failed to find..."",
+                      ""locations"": [
+                        {
+                          ""end"": 137,
+                          ""start"": 47
+                        }
+                      ],
+                      ""path"": [
+                        ""feeds""
+                      ],
+                      ""extensions"": {
+                        ""code"": ""COMPLETEVALUE"",
+                        ""remoteError"": {
+                          ""data"": null,
+                          ""errors"": [
+                            {
+                              ""message"": ""failed to find...""
+                            }
+                          ],
+                          ""extensions"": null
+                        }
+                      }
+                    }
+                  ]
+                }
+            ");
+        }
+    }
+}
