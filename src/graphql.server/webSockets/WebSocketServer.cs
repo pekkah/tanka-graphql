c87b7d84332d39cc813919742f7732199628ce58
Pekka Heikura <pekkah@gmail.com>
2019-04-27 12:26:32 +0300

    Use scope around websocket connection

--- src/graphql.server/webSockets/WebSocketServer.cs
+++ src/graphql.server/webSockets/WebSocketServer.cs
@@ -2,23 +2,20 @@
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using tanka.graphql.execution;
 
 namespace tanka.graphql.server.webSockets
 {
     public class WebSocketServer
     {
         private readonly ILoggerFactory _loggerFactory;
-        private readonly IProtocolHandler _protocol;
         private ILogger<WebSocketServer> _logger;
 
         public WebSocketServer(
-            ILoggerFactory loggerFactory,
-            IProtocolHandler protocol)
+            ILoggerFactory loggerFactory)
         {
             _loggerFactory = loggerFactory;
-            _protocol = protocol;
             _logger = loggerFactory.CreateLogger<WebSocketServer>();
         }
 
@@ -28,11 +25,16 @@ namespace tanka.graphql.server.webSockets
         public async Task ProcessRequestAsync(HttpContext context)
         {
             MessageServer messageServer = null;
+            var scope = context.RequestServices.GetRequiredService<IServiceScopeFactory>()
+                .CreateScope();
+
             try
             {
                 var connection = new WebSocketConnection(_loggerFactory);
-                messageServer = new SubscriptionServer(_protocol);
-                
+                var protocol = scope.ServiceProvider.GetRequiredService<IProtocolHandler>();
+
+                messageServer = new SubscriptionServer(protocol);
+
                 Clients.TryAdd(context.Connection, messageServer);
                 var run = messageServer.RunAsync(connection, context.RequestAborted);
                 await connection.ProcessRequestAsync(context);
@@ -51,6 +53,8 @@ namespace tanka.graphql.server.webSockets
                     s.Complete();
                     await s.Completion;
                 }
+
+                scope.Dispose();
             }
         }
     }
