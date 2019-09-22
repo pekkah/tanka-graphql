b068f7bea0bb0cc1c868be2699144077e521af80
Pekka Heikura <pekkah@gmail.com>
2019-09-22 21:14:00 +0300

    Fix bug #339 - input field defaultValue was coeced to object[] { null } instead of null (#340)

---
+++ tests/graphql.tests/bug/Bug_339.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tanka.graphql.introspection;
+using tanka.graphql.schema;
+using tanka.graphql.sdl;
+using Xunit;
+
+namespace tanka.graphql.tests.bug
+{
+    public class Bug_339
+    {
+        [Fact]
+        public async Task Introspection_should_pass()
+        {
+            /* Given */
+            var schema = new SchemaBuilder()
+                .Sdl(@"
+                        input InputItem {
+                          foo: [String]
+                        }
+
+                        type Mutation {
+                          createItem(input: InputItem!): ID
+                        }
+
+                        type Query {
+                            field : String
+                        }
+
+                        schema {
+                            query: Query
+                            mutation: Mutation
+                        }
+                ")
+                .Build();
+
+            var introspectionSchema = Introspect.Schema(schema);
+
+            /* When */
+            var result = await Executor.ExecuteAsync(new ExecutionOptions()
+            {
+                Schema = introspectionSchema,
+                Document = Parser.ParseDocument(Introspect.DefaultQuery)
+            });
+
+            /* Then */
+            var types = (List<object>)result.Select("__schema", "types");
+
+            foreach (var type in types)
+            {
+                var typeDictionary = (Dictionary<string, object>) type;
+
+                if ((string) typeDictionary["name"] == "InputItem")
+                {
+                    var inputFields = (List<object>)typeDictionary["inputFields"];
+
+                    foreach (var inputField in inputFields)
+                    {
+                        var inputFieldDictionary = (Dictionary<string, object>) inputField;
+
+                        if ((string) inputFieldDictionary["name"] == "foo")
+                        {
+                            var defaultValue = inputFieldDictionary["defaultValue"];
+                            Assert.Null(defaultValue);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
