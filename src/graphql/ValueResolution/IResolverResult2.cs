94f5a96d82b08a2cadf0682bc5d1ad9438a234cd
Pekka Heikura <pekkah@gmail.com>
2019-11-24 23:12:28 +0200

    wip

---
+++ src/graphql/ValueResolution/IResolverResult2.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace Tanka.GraphQL.ValueResolution
+{
+    public interface IResolverResult2
+    {
+        ValueTask<object> CompleteValueAsync(
+            IResolverContext context);
+    }
+
+    public class ResolverResultBase : IResolverResult2
+    {
+        private readonly object _value;
+
+        public ResolverResultBase(object value)
+        {
+            _value = value;
+        }
+
+        public ValueTask<object> CompleteValueAsync(IResolverContext context)
+        {
+            return default;
+        }
+    }
+}
