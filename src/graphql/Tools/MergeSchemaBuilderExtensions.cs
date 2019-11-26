db07be4933038a74f8ed20a55e6e763a118f125b
Pekka Heikura <pekkah@gmail.com>
2019-11-26 21:00:24 +0200

    Include ignored files

---
+++ src/graphql/Tools/MergeSchemaBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Tanka.GraphQL.SchemaBuilding;
+using Tanka.GraphQL.TypeSystem;
+
+namespace Tanka.GraphQL.Tools
+{
+    public static class MergeSchemaBuilderExtensions
+    {
+        public static SchemaBuilder Merge(this SchemaBuilder builder, params ISchema[] schemas)
+        {
+            MergeTool.Schemas(builder, schemas);
+            return builder;
+        }
+    }
+}
