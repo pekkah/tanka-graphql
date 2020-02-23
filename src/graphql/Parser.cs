98b10932871559d8f9440bb282f7c3361d8d654b
Pekka Heikura <pekkah@gmail.com>
2020-02-23 14:03:41 +0200

    wip: parser read imports

--- src/graphql/Parser.cs
+++ src/graphql/Parser.cs
@@ -1,13 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using GraphQLParser;
 using GraphQLParser.AST;
+using Tanka.GraphQL.TypeSystem;
 
 namespace Tanka.GraphQL
 {
     public static class Parser
     {
         /// <summary>
-        ///     Parse <see cref="document"/> into <see cref="GraphQLDocument"/>
+        ///     Parse <see cref="document" /> into <see cref="GraphQLDocument" />
         /// </summary>
         /// <param name="document"></param>
         /// <returns></returns>
@@ -19,7 +22,7 @@ namespace Tanka.GraphQL
         }
 
         /// <summary>
-        ///     Parse <see cref="document"/> into <see cref="GraphQLDocument"/>
+        ///     Parse <see cref="document" /> into <see cref="GraphQLDocument" />
         /// </summary>
         /// <param name="document"></param>
         /// <returns></returns>
@@ -32,5 +35,48 @@ namespace Tanka.GraphQL
                 return parser.Parse(new Source(document));
             });
         }
+
+        public static async Task<GraphQLDocument> ParseDocumentAsync(string document, ParserOptions options)
+        {
+            var imports = ParseImports(document);
+
+            await Task.Yield();
+            return document;
+        }
+
+        private static IEnumerable<DirectiveInstance> ParseImports(string document)
+        {
+            var reader = new StringReader(document);
+
+            while (true)
+            {
+                var line = reader.ReadLine();
+
+                if (line == null)
+                    break;
+
+                if (line.StartsWith("# @import"))
+                    yield return ReadImport(line);
+                else if (line.StartsWith("#"))
+                    continue;
+                else
+                    break;
+            }
+        }
+
+        private static DirectiveInstance ReadImport(string commentLineWithImport)
+        {
+            return null;
+        }
+    }
+
+    public class ParserOptions
+    {
+        public List<IDocumentImportProvider> ImportProviders { get; set; }
+    }
+
+    public interface IDocumentImportProvider
+    {
+        bool CanImport(DirectiveInstance import);
     }
 }
---
+++ tests/graphql.tests/ParserFacts.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQLParser.AST;
+using NSubstitute;
+using Tanka.GraphQL.TypeSystem;
+using Xunit;
+
+namespace Tanka.GraphQL.Tests
+{
+    public class ParserFacts
+    {
+        [Fact]
+        public async Task Use_import_provider()
+        {
+            /* Given */
+            var provider = Substitute.For<IDocumentImportProvider>();
+            provider.CanImport(null).ReturnsForAnyArgs(true);
+            
+            var sdl = @"
+                # @import(path: ""./Imported"")
+
+                type Query {
+                    field: Imported
+                }
+                ";
+
+
+            /* When */
+            var document = await Parser.ParseDocumentAsync(
+                sdl,
+                new ParserOptions
+                {
+                    ImportProviders = new List<IDocumentImportProvider>
+                    {
+                        provider
+                    }
+                });
+
+            /* Then */
+            Assert.Single(
+                document.Definitions.OfType<GraphQLObjectTypeDefinition>(),
+                objectTypeDef => objectTypeDef.Name.Value == "Imported");
+        }
+    }
+}
