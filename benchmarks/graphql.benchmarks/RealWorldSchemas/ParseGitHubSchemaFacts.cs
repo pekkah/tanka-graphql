c0c9694f88e40e49a5ead914a4906248a183a625
Pekka Heikura <pekkah@gmail.com>
2020-03-19 09:02:34 +0200

    Add GitHub schema as benchmark and as a smoke test

---
+++ benchmarks/graphql.benchmarks/RealWorldSchemas/ParseGitHubSchemaFacts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace Tanka.GraphQL.Benchmarks.RealWorldSchemas
+{
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    [MemoryDiagnoser]
+    [MarkdownExporterAttribute.GitHub]
+    public class ParseGitHubSchemaBenchmark
+    {
+        public byte[] GitHubBytes { get; private set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            GitHubBytes = File.ReadAllBytes("RealWorldSchemas/github.graphql");
+        }
+
+        [Benchmark]
+        public void GitHub()
+        {
+            var parser = Language.Parser.Create(GitHubBytes);
+            var typeSystem = parser.ParseTypeSystemDocument();
+
+            if (typeSystem.TypeDefinitions == null || !typeSystem.TypeDefinitions.Any())
+            {
+                throw new Exception("It has types");
+            }
+        }
+    }
+}
---
+++ tests/graphql.language.tests/RealWorldSchemas/ParseGitHubSchemaFacts.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Xunit;
+
+namespace Tanka.GraphQL.Language.Tests.RealWorldSchemas
+{
+    public class ParseGitHubSchemaFacts
+    {
+        public ParseGitHubSchemaFacts()
+        {
+            GitHubBytes = File.ReadAllBytes("RealWorldSchemas/github.graphql");
+        }
+
+        public byte[] GitHubBytes { get; }
+
+        [Fact]
+        public void Parse()
+        {
+            var parser = Parser.Create(GitHubBytes);
+            var typeSystem = parser.ParseTypeSystemDocument();
+
+            Assert.NotNull(typeSystem.TypeDefinitions);
+            Assert.NotEmpty(typeSystem.TypeDefinitions);
+        }
+    }
+}
