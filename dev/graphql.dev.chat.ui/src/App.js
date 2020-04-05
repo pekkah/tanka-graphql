10339c7de574465bd8fb26a59f25ba9882b10bea
Pekka Heikura <pekkah@gmail.com>
2020-04-05 16:56:22 +0300

    NPM Upgrade * Upgrade packages * Switch dev.*ui* to graphql as playground has issues with newer packages

--- dev/graphql.dev.chat.ui/src/App.js
+++ dev/graphql.dev.chat.ui/src/App.js
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { Provider } from "react-redux";
-import { store, Playground } from "graphql-playground-react";
 import { TankaClient, TankaLink } from "@tanka/tanka-graphql-server-link";
 import { RetryLink } from "apollo-link-retry";
 import { ApolloLink } from 'apollo-link';
+import GraphiQL from 'graphiql';
+import { parse } from 'graphql';
+import { execute } from 'apollo-link';
 
 const client = new TankaClient('https://localhost:5000/graphql', {});
 const tankaLink = new TankaLink(client);
@@ -13,36 +14,15 @@ const link = ApolloLink.from([
   tankaLink
 ]);
 
-const settings  = {
-  'editor.cursorShape': 'line', // possible values: 'line', 'block', 'underline'
-  'editor.fontFamily': `'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace`,
-  'editor.fontSize': 14,
-  'editor.reuseHeaders': true, // new tab reuses headers from last tab
-  'editor.theme': 'dark', // possible values: 'dark', 'light'
-  'general.betaUpdates': false,
-  'prettier.printWidth': 80,
-  'prettier.tabWidth': 2,
-  'prettier.useTabs': false,
-  'request.credentials': 'omit', // possible values: 'omit', 'include', 'same-origin'
-  'schema.polling.enable': true, // enables automatic schema polling
-  'schema.polling.endpointFilter': '*', // endpoint filter for schema polling
-  'schema.polling.interval': 10000, // schema polling interval in ms
-  'schema.disableComments': false,
-  'tracing.hideTracingResponse': true,
+const fetcher = (operation) => {
+  operation.query = parse(operation.query);
+  return execute(link, operation);
 };
 
 class Home extends React.Component {
-  createLink = (session) => {
-    return {
-      link: link
-    };
-  };
-
   render() {
     return (
-      <Provider store={store}>
-        <Playground createApolloLink={this.createLink} settings={settings} />
-      </Provider>
+      <GraphiQL fetcher={fetcher}/>
     );
   }
 }
