57222f1ef04de16b18f7b648e9fd555fe9202d15
Pekka Heikura <pekkah@gmail.com>
2020-11-20 13:56:11 +0200

    net5.0 (#747)
    * Add net5.0 targets

---
+++ dev/graphql.dev.chat.web.3.1/Controllers/QueryController.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tanka.GraphQL.Samples.Chat.Web.GraphQL;
+using Tanka.GraphQL.Server;
+using static Tanka.GraphQL.Parser;
+
+namespace Tanka.GraphQL.Samples.Chat.Web.Controllers
+{
+    [Route("api/graphql")]
+    public class QueryController : Controller
+    {
+        private readonly IQueryStreamService _queryStreamService;
+
+        public QueryController(IQueryStreamService queryStreamService)
+        {
+            _queryStreamService = queryStreamService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] OperationRequest request)
+        {
+            var stream = await _queryStreamService.QueryAsync(new Query
+            {
+                Document = ParseDocument(request.Query),
+                Variables = request.Variables,
+                OperationName = request.OperationName
+            }, Request.HttpContext.RequestAborted);
+
+            var result = await stream.Reader.ReadAsync();
+
+            return Ok(result);
+        }
+    }
+}
