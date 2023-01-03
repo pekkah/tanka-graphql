1cb64f1c4c6702239c8eb837831ffef0b341e9ec
Pekka Heikura <pekkah@gmail.com>
2023-01-03 16:45:00 +0200

    QueryPlanneri wip

--- tests/graphql.tests/Planner/QueryPlannerFacts.cs
+++ tests/graphql.tests/Planner/QueryPlannerFacts.cs
@@ -15,13 +15,18 @@ using Tanka.GraphQL.TypeSystem;
 using Xunit;
 using System.IO;
 using Tanka.GraphQL.Introspection;
+using Tanka.GraphQL.Tests.Data;
+using Tanka.GraphQL.ValueResolution;
+using FieldDefinition = Tanka.GraphQL.Language.Nodes.TypeSystem.FieldDefinition;
+using TypeDefinition = Tanka.GraphQL.Language.Nodes.TypeSystem.TypeDefinition;
+using System.Xml.Linq;
 
 namespace Tanka.GraphQL.Tests.Planner;
 
 public class QueryPlanExecutorFacts
 {
     [Fact]
-    public async Task Plan()
+    public async Task Simple_Scalar()
     {
         /* Given */
         var schema = await new SchemaBuilder()
@@ -31,7 +36,13 @@ public class QueryPlanExecutorFacts
                 version: String!
             }
             """)
-            .Build(new SchemaBuildOptions());
+            .Build(new ResolversMap()
+            {
+                ["Query"] = new()
+                {
+                    {"version", ctx => ResolveSync.As("1.0")}
+                }
+            });
 
         ExecutableDocument query = """
             {
@@ -41,10 +52,76 @@ public class QueryPlanExecutorFacts
 
         /* When */
         var plan = new QueryPlanner(schema).Plan(query);
-        var result = new QueryPlanExecutor().Execute(plan);
+        await using var enumerator = new QueryPlanExecutor(schema)
+            .Execute(plan, null)
+            .GetAsyncEnumerator();
+
+        await enumerator.MoveNextAsync();
+        var result = enumerator.Current;
 
         /* Then */
+        result.ShouldMatchJson("""
+            {
+               "data": {
+                  "version": "1.0"
+              }
+            }
+            """);
+    }
 
+    [Fact]
+    public async Task Object_with_ScalarField()
+    {
+        /* Given */
+        var schema = await new SchemaBuilder()
+            .Add("""
+            type System 
+            {
+                version: String!
+            }
+
+            type Query 
+            {
+                system: System!
+            }
+            """)
+            .Build(new ResolversMap()
+            {
+                ["Query"] = new()
+                {
+                    {"system", ctx => ResolveSync.As("system")}
+                },
+                ["System"] = new()
+                {
+                    {"version", ctx => ResolveSync.As("1.0")}
+                }
+            });
+
+        ExecutableDocument query = """
+            {
+                system {
+                    version
+                }
+            }
+            """;
+
+        /* When */
+        var plan = new QueryPlanner(schema).Plan(query);
+        await using var enumerator = new QueryPlanExecutor(schema)
+            .Execute(plan, null)
+            .GetAsyncEnumerator();
+
+        await enumerator.MoveNextAsync();
+        var result = enumerator.Current;
+
+        /* Then */
+        result.ShouldMatchJson("""
+            {
+               "data": {
+                  "version": "1.0"
+              }
+            }
+            """);
     }
 }
 
@@ -58,28 +135,91 @@ public class QueryPlanExecutorContext
 
 public class QueryPlanExecutor
 {
-    public async IAsyncEnumerable<ExecutionResult> Execute(QueryPlan plan)
+    public ISchema Schema { get; }
+
+    public QueryPlanExecutor(ISchema schema)
+    {
+        Schema = schema;
+    }
+
+    public async IAsyncEnumerable<ExecutionResult> Execute(
+        QueryPlan plan,
+        object? initialValue)
     {
         var root = plan.Root;
 
-        var nodes = new Queue<QueryPlanNode>();
-        nodes.Enqueue(root);
+        var nodes = new Queue<SelectionSetNode>();
+        nodes.Enqueue(root.RootSelectionSetNode);
 
-        while (nodes.TryDequeue(out var node))
+        var resultMap = new Dictionary<string, object>();
+        var selectionSetResultMap = resultMap;
+        while (nodes.TryDequeue(out var selectionSetNode))
         {
-            var task = node switch
+            foreach (var node in selectionSetNode.Children)
             {
-                SelectionSetNode objectNode => ExecuteObjectNode(plan, objectNode),
-                _ => throw new InvalidOperationException("Root should be object?")
-            };
-
-            await task;
+                switch (node)
+                {
+                    case ScalarNode scalarNode:
+                        var result = await ExecuteScalarNode(
+                            selectionSetNode,
+                            scalarNode,
+                            initialValue,
+                            new Dictionary<string, object>());
+                        selectionSetResultMap[scalarNode.ResponseKey] = result;
+                    break;
+                    case SelectionSetNode subSelectionSetNode:
+                        nodes.Enqueue(subSelectionSetNode);
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
         }
-
-        yield break;
+        
+        yield return new ExecutionResult()
+        {
+            Data = resultMap
+        };
     }
 
-    private Task ExecuteObjectNode(QueryPlan plan, SelectionSetNode selectionSetNode)
+    private async Task<object> ExecuteScalarNode(
+        SelectionSetNode selectionSetNode, 
+        ScalarNode scalarNode, 
+        object? objectValue,
+        Dictionary<string, object> coercedVariableValues)
+    {
+        var fieldSelection = scalarNode.Fields.First();
+        var argumentValues = ArgumentCoercion.CoerceArgumentValues(
+            Schema,
+            selectionSetNode.ObjectDefinition,
+            scalarNode.Fields.First(),
+            coercedVariableValues);
+
+        var resolver = Schema.GetResolver(
+            selectionSetNode.ObjectDefinition.Name,
+            scalarNode.FieldName);
+
+        if (resolver is null)
+            return null;
+
+        var resolverContext = new ResolverContext(
+            selectionSetNode.ObjectDefinition,
+            objectValue,
+            scalarNode.FieldDefinition,
+            fieldSelection,
+            scalarNode.Fields!,
+            argumentValues,
+            scalarNode.Path,
+            null!); //todo: we need alternative for this?
+
+        var resolvedValue = await resolver(resolverContext);
+
+        return resolvedValue.Value;
+    }
+
+    private Task ExecuteObjectNode(
+        QueryPlan plan, 
+        SelectionSetNode selectionSetNode)
     {
         return null;
     }
@@ -122,16 +262,29 @@ public class QueryPlanner
 
         foreach (var (responseKey, fields) in groupedFieldSet)
         {
-            children.Add(PlanField(responseKey, fields, objectDefinition, path.Fork()));
+            children.Add(PlanField(document, responseKey, fields, objectDefinition, path.Fork()));
         }
 
-        return new QueryPlan(null);
+        return new QueryPlan(new RootNode()
+        {
+            Path = path,
+            ObjectDefinition = objectDefinition,
+            OperationDefinition = operation,
+            RootSelectionSetNode = new SelectionSetNode()
+            {
+                Children = children,
+                GroupedFieldSet = groupedFieldSet,
+                ObjectDefinition = objectDefinition,
+                SelectionSet = selectionset
+            }
+        });
     }
 
     private QueryPlanNode PlanField(
-        string responseKey, 
-        IReadOnlyList<FieldSelection> fields, 
-        ObjectDefinition objectDefinition, 
+        ExecutableDocument document, 
+        string responseKey,
+        IReadOnlyList<FieldSelection> fields,
+        ObjectDefinition objectDefinition,
         NodePath path)
     {
         var fieldName = fields.First().Name;
@@ -146,31 +299,97 @@ public class QueryPlanner
         if (field is not null)
         {
             var fieldType = field.Type;
-            var resolver = Schema.GetResolver(objectDefinition.Name, fieldName);
+            var fieldTypeDefinition = Ast.UnwrapAndResolveType(Schema, fieldType);
+            var isComplexType = fieldTypeDefinition?.Kind is NodeKind.ObjectDefinition or NodeKind.InterfaceDefinition or NodeKind.UnionDefinition;
 
-            //todo: do I create somekind of russian doll model for the definition as it an be List, NonNull etc
-            //var fieldTypeDefinition = ???
+            if (!isComplexType)
+            {
+                return new ScalarNode()
+                {
+                    Path = path,
+                    FieldDefinition = field,
+                    UnwrappedFieldType = fieldTypeDefinition,
+                    ObjectDefinition = objectDefinition,
+                    FieldName = fieldName,
+                    Fields = fields,
+                    ResponseKey = responseKey,
+                };
+            }
+
+            var subSelectionSet = SelectionSets.MergeSelectionSets(fields);
+            var groupedFieldSet = SelectionSets.CollectFields(
+                Schema,
+                document,
+                objectDefinition,
+                subSelectionSet);
+
+            var children = new List<QueryPlanNode>();
+
+            foreach (var (subResponseKey, subFields) in groupedFieldSet)
+            {
+                children.Add(PlanField(
+                    document,
+                    subResponseKey, 
+                    subFields, 
+                    fieldTypeDefinition as ObjectDefinition, 
+                    path.Fork()));
+            }
+
+            return new SelectionSetNode()
+            {
+                Children = children,
+                GroupedFieldSet = groupedFieldSet,
+                ObjectDefinition = fieldTypeDefinition as ObjectDefinition,
+                SelectionSet = subSelectionSet
+            };
+
         }
 
         return null;
     }
 }
 
-public record QueryPlan(QueryPlanNode Root);
+public record QueryPlan(RootNode Root);
 
 public class QueryPlanNode
 {
-    public QueryPlanNode[] Children { get; set; }
+}
+
+public class ScalarNode : QueryPlanNode
+{
+    public NodePath Path { get; set; }
+    
+    public FieldDefinition FieldDefinition { get; set; }
+    
+    public TypeDefinition UnwrappedFieldType { get; set; }
+    
+    public ObjectDefinition ObjectDefinition { get; set; }
+    
+    public string FieldName { get; set; }
+    
+    public IReadOnlyList<FieldSelection> Fields { get; set; }
+    
+    public string ResponseKey { get; set; }
 }
 
 public class SelectionSetNode : QueryPlanNode
 {
-    public Task Execute(object? objectValue)
-    {
-        return Task.CompletedTask;
-    }
-
     public SelectionSet SelectionSet { get; set; }
+    
     public ObjectDefinition ObjectDefinition { get; set; }
+    
     public IReadOnlyDictionary<string, List<FieldSelection>> GroupedFieldSet { get; set; }
+
+    public IReadOnlyList<QueryPlanNode> Children { get; set; }
+}
+
+public class RootNode : QueryPlanNode
+{
+    public NodePath Path { get; set; }
+    
+    public ObjectDefinition ObjectDefinition { get; set; }
+    
+    public OperationDefinition OperationDefinition { get; set; }
+
+    public SelectionSetNode RootSelectionSetNode { get; set; }
 }
