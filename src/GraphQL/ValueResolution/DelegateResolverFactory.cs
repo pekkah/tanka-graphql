0723716bc84a69289f45d3b9c3e1d735d114344c
Pekka Heikura <pekkah@gmail.com>
2023-03-19 14:03:23 +0200

    Parameter binding from ResolverContext

--- src/GraphQL/ValueResolution/DelegateResolverFactory.cs
+++ src/GraphQL/ValueResolution/DelegateResolverFactory.cs
@@ -1,12 +1,23 @@
-﻿using System.Linq.Expressions;
-using Tanka.GraphQL.ValueResolution;
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
+namespace Tanka.GraphQL.ValueResolution;
 
 public static class DelegateResolverFactory
 {
+    private static readonly ParameterExpression ContextParam = Expression.Parameter(typeof(ResolverContext), "context");
+    private static readonly IReadOnlyDictionary<string, Expression> ContextParamProperties = typeof(ResolverContext)
+        .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+        .ToDictionary(p => p.Name.ToLowerInvariant(), p => (Expression)Expression.Property(ContextParam, p));
+
     public static Resolver Create(Delegate resolverDelegate)
     {
-        var contextParam = Expression.Parameter(typeof(ResolverContext), "context");
+#if DEBUG
+        Trace.WriteLine($"Available context parameters:\n {string.Join(',', ContextParamProperties.Select(p => string.Concat($"{p.Key}: {p.Value.Type}")))}");
+#endif
+
         var invokeMethod = resolverDelegate.Method;
 
         Expression instanceExpression = null;
@@ -15,10 +26,50 @@ public static class DelegateResolverFactory
             instanceExpression = Expression.Constant(resolverDelegate.Target);
         }
 
+        IEnumerable<Expression> argumentsExpressions = invokeMethod.GetParameters()
+            .Select(p =>
+            {
+                if (p.ParameterType == typeof(ResolverContext))
+                {
+                    return ContextParam;
+                }
+
+                if (p.Name is not null)
+                    if (ContextParamProperties.TryGetValue(p.Name.ToLowerInvariant(), out var propertyExpression))
+                    {
+                        if (p.ParameterType == propertyExpression.Type)
+                            return propertyExpression;
+
+                        return Expression.Convert(propertyExpression, p.ParameterType);
+                    }
+
+                var serviceProviderProperty = Expression.Property(ContextParam, "RequestServices");
+                var getServiceMethodInfo = typeof(ServiceProviderServiceExtensions)
+                    .GetMethods()
+                    .FirstOrDefault(m => m.Name == nameof(ServiceProviderServiceExtensions.GetRequiredService)
+                                         && m.GetParameters().Length == 1
+                                         && m.GetParameters()[0].ParameterType == typeof(IServiceProvider)
+                                         && m.IsGenericMethodDefinition);
+
+                if (getServiceMethodInfo is null)
+                    throw new InvalidOperationException("Could not find GetRequiredService method");
+
+                var serviceType = p.ParameterType;
+                var genericMethodInfo = getServiceMethodInfo.MakeGenericMethod(serviceType);
+                var getRequiredServiceCall = (Expression)Expression.Call(
+                    null,
+                    genericMethodInfo,
+                    serviceProviderProperty);
+
+                return (Expression)Expression.Block(
+                    getRequiredServiceCall
+                );
+            });
+
         var invokeExpression = Expression.Call(
             instanceExpression,
             invokeMethod,
-            contextParam
+            argumentsExpressions
         );
 
         Expression valueTaskExpression;
@@ -45,14 +96,12 @@ public static class DelegateResolverFactory
             throw new ArgumentException($"Unsupported delegate return type {invokeMethod.ReturnType}", nameof(resolverDelegate));
         }
 
+
         var lambda = Expression.Lambda<Resolver>(
             valueTaskExpression,
-            contextParam
+            ContextParam
         );
 
         return lambda.Compile();
     }
-
-
-
 }
--- tests/graphql.tests/ValueResolution/DelegateResolverFactoryFacts.cs
+++ tests/graphql.tests/ValueResolution/DelegateResolverFactoryFacts.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Tanka.GraphQL.Language.Nodes;
 using Tanka.GraphQL.Language.Nodes.TypeSystem;
 using Tanka.GraphQL.ValueResolution;
 using Xunit;
@@ -44,6 +47,189 @@ public class DelegateResolverFactoryFacts
     }
 
     [Fact]
+    public async Task ReturnValue_is_Task_with_context_and_a_service_parameter()
+    {
+        /* Given */
+        static async Task AsyncResolver(ResolverContext context, IMyDependency dep1)
+        {
+            await Task.Delay(0);
+            context.ResolvedValue = dep1;
+        }
+
+        Delegate resolverDelegate = AsyncResolver;
+
+        /* When */
+        Resolver resolver = DelegateResolverFactory.Create(resolverDelegate);
+
+        /* Then */
+        var context = new ResolverContext
+        {
+            ObjectDefinition = null,
+            ObjectValue = null,
+            Field = null,
+            Selection = null,
+            Fields = null,
+            ArgumentValues = null,
+            Path = null,
+            QueryContext = new QueryContext()
+            {
+                RequestServices = new ServiceCollection()
+                    .AddSingleton<IMyDependency, MyDependency>()
+                    .BuildServiceProvider()
+            }
+        };
+
+        await resolver(context);
+
+        Assert.NotNull(context.ResolvedValue);
+        Assert.IsAssignableFrom<IMyDependency>(context.ResolvedValue);
+    }
+
+    [Fact]
+    public async Task ReturnValue_is_Task_with_context_and_ObjectValue_parameter()
+    {
+        /* Given */
+        static async Task AsyncResolver(ResolverContext context, object? objectValue)
+        {
+            await Task.Delay(0);
+            context.ResolvedValue = objectValue;
+        }
+
+        Delegate resolverDelegate = AsyncResolver;
+
+        /* When */
+        Resolver resolver = DelegateResolverFactory.Create(resolverDelegate);
+
+        /* Then */
+        var context = new ResolverContext
+        {
+            ObjectDefinition = null,
+            ObjectValue = "test",
+            Field = null,
+            Selection = null,
+            Fields = null,
+            ArgumentValues = null,
+            Path = null,
+            QueryContext = null
+        };
+
+        await resolver(context);
+
+        Assert.NotNull(context.ResolvedValue);
+        Assert.Equal("test", context.ResolvedValue);
+    }
+
+    [Fact]
+    public async Task ReturnValue_is_Task_with_context_and_TypedObjectValue_parameter()
+    {
+        /* Given */
+        static async Task AsyncResolver(ResolverContext context, string? objectValue)
+        {
+            await Task.Delay(0);
+            context.ResolvedValue = objectValue;
+        }
+
+        Delegate resolverDelegate = AsyncResolver;
+
+        /* When */
+        Resolver resolver = DelegateResolverFactory.Create(resolverDelegate);
+
+        /* Then */
+        var context = new ResolverContext
+        {
+            ObjectDefinition = null,
+            ObjectValue = "test",
+            Field = null,
+            Selection = null,
+            Fields = null,
+            ArgumentValues = null,
+            Path = null,
+            QueryContext = null
+        };
+
+        await resolver(context);
+
+        Assert.NotNull(context.ResolvedValue);
+        Assert.Equal("test", context.ResolvedValue);
+    }
+
+    [Fact]
+    public async Task ReturnValue_is_Task_with_all_context_members()
+    {
+        /* Given */
+        bool called = false;
+        async Task AsyncResolver(
+            ObjectDefinition objectDefinition, 
+            string objectValue,
+            FieldDefinition field,
+            FieldSelection selection,
+            IReadOnlyCollection<FieldSelection> fields,
+            IReadOnlyDictionary<string, object?> argumentValues,
+            NodePath path,
+            QueryContext queryContext)
+        {
+            await Task.Delay(0);
+            called = true;
+        }
+
+        Delegate resolverDelegate = AsyncResolver;
+
+        /* When */
+        Resolver resolver = DelegateResolverFactory.Create(resolverDelegate);
+
+        /* Then */
+        var context = new ResolverContext
+        {
+            ObjectDefinition = null,
+            ObjectValue = "test",
+            Field = null,
+            Selection = null,
+            Fields = null,
+            ArgumentValues = null,
+            Path = null,
+            QueryContext = null
+        };
+
+        await resolver(context);
+
+        Assert.True(called);
+    }
+
+    [Fact]
+    public async Task ReturnValue_is_Task_no_parameters()
+    {
+        /* Given */
+        var called = false;
+        Task AsyncResolver()
+        {
+            called = true;
+            return Task.CompletedTask;
+        }
+
+        Delegate resolverDelegate = AsyncResolver;
+
+        /* When */
+        Resolver resolver = DelegateResolverFactory.Create(resolverDelegate);
+
+        /* Then */
+        var context = new ResolverContext
+        {
+            ObjectDefinition = null,
+            ObjectValue = null,
+            Field = null,
+            Selection = null,
+            Fields = null,
+            ArgumentValues = null,
+            Path = null,
+            QueryContext = null
+        };
+
+        await resolver(context);
+
+        Assert.True(called);
+    }
+
+    [Fact]
     public async Task ReturnValue_is_ValueTask()
     {
         /* Given */
@@ -75,6 +261,40 @@ public class DelegateResolverFactoryFacts
 
         Assert.NotNull(context.ResolvedValue);
         Assert.True((bool)context.ResolvedValue);
+    }
+
+    [Fact]
+    public async Task ReturnValue_is_ValueTask_no_parameters()
+    {
+        /* Given */
+        var called = false;
+        ValueTask AsyncResolver()
+        {
+            called  = true;
+            return default;
+        }
+
+        Delegate resolverDelegate = AsyncResolver;
+
+        /* When */
+        Resolver resolver = DelegateResolverFactory.Create(resolverDelegate);
+
+        /* Then */
+        var context = new ResolverContext
+        {
+            ObjectDefinition = null,
+            ObjectValue = null,
+            Field = null,
+            Selection = null,
+            Fields = null,
+            ArgumentValues = null,
+            Path = null,
+            QueryContext = null
+        };
+
+        await resolver(context);
+
+        Assert.True(called);
     }
 
     [Fact]
@@ -111,16 +331,49 @@ public class DelegateResolverFactoryFacts
     }
 
     [Fact]
+    public async Task ReturnValue_is_void_no_parameters()
+    {
+        /* Given */
+        var called = false;
+        void Resolver()
+        {
+            called = true;
+        }
+
+        Delegate resolverDelegate = Resolver;
+
+        /* When */
+        Resolver resolver = DelegateResolverFactory.Create(resolverDelegate);
+
+        /* Then */
+        var context = new ResolverContext
+        {
+            ObjectDefinition = null,
+            ObjectValue = null,
+            Field = null,
+            Selection = null,
+            Fields = null,
+            ArgumentValues = null,
+            Path = null,
+            QueryContext = null
+        };
+
+        await resolver(context);
+
+        Assert.True(called);
+    }
+
+    [Fact(Skip = "todo")]
     public async Task ReturnValue_is_TaskT()
     {
         /* Given */
-        static async Task<bool> SyncResolver()
+        static async Task<bool> AsyncResolver()
         {
             await Task.Delay(0);
             return true;
         }
 
-        Delegate resolverDelegate = SyncResolver;
+        Delegate resolverDelegate = AsyncResolver;
 
         /* When */
         Resolver resolver = DelegateResolverFactory.Create(resolverDelegate);
