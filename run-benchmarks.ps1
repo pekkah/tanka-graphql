818573884a798d24c738e360d705351fc74a7185
Pekka Heikura <pekkah@gmail.com>
2023-04-03 20:10:25 +0300

    [chore}: Renames (#1612)

--- run-benchmarks.ps1
+++ run-benchmarks.ps1
@@ -50,7 +50,7 @@ $IsPreRelease = $PreReleaseTag -ne ''
 
 "----------------------------------------"
 "Benchmarks"
-$BechmarkCmd = "dotnet run --project ./benchmarks/graphql.benchmarks/graphql.benchmarks.csproj -c Release --framework net7.0 -- -i -m --filter *execution*"
+$BechmarkCmd = "dotnet run --project ./benchmarks/GraphQL.Benchmarks/GraphQL.Benchmarks.csproj -c Release --framework net7.0 -- -i -m --filter *execution*"
 
 if ($IsPreRelease) {
     $BechmarkCmd += " --job short"
