5fa48aafc554236e9b28d3b4f7cdf107e1917bbb
Pekka Heikura <pekkah@gmail.com>
2024-02-12 14:23:37 +0200

    Check if value is INamedType and use that

--- src/GraphQL/ValueResolution/ResolverContext.cs
+++ src/GraphQL/ValueResolution/ResolverContext.cs
@@ -1,19 +1,52 @@
-﻿using Tanka.GraphQL.Language.Nodes.TypeSystem;
+﻿using System.Diagnostics;
+
+using Tanka.GraphQL.Language.Nodes.TypeSystem;
 
 namespace Tanka.GraphQL.ValueResolution;
 
 public class ResolverContext : ResolverContextBase
 {
+    public ResolverContext()
+    {
+        ResolveAbstractType = DefaultIsTypeOf;
+    }
+
     public object? ResolvedValue { get; set; }
 
     public object? CompletedValue { get; set; }
 
-    public Func<TypeDefinition, object?, TypeDefinition>? ResolveAbstractType { get; set; } = DefaultIsTypeOf;
+    public Func<TypeDefinition, object?, TypeDefinition>? ResolveAbstractType { get; set; }
 
-    private static TypeDefinition DefaultIsTypeOf(TypeDefinition abstractType, object? value)
+    private TypeDefinition DefaultIsTypeOf(TypeDefinition abstractType, object? value)
     {
+        if (value is INamedType namedType) 
+        {
+            var typeDefinition = Schema.GetRequiredNamedType<TypeDefinition>(namedType.__Typename);
+            ValidateAbstractType(abstractType, typeDefinition);
+            return typeDefinition;
+        }
+
         throw new InvalidOperationException(
             $"Cannot resolve actual type of value of abstract type {abstractType.Name}. " +
-            "Use the context.IsTypeOf function to map from value to actual schema type");
+            "Use the context.ResolveAbstractType function to map from value to actual schema type");
+    }
+
+    [Conditional("DEBUG")]
+    private void ValidateAbstractType(TypeDefinition abstractType, TypeDefinition typeDefinition)
+    {
+        IReadOnlyList<TypeDefinition> possibleTypes = abstractType switch
+        {
+            UnionDefinition unionDefinition => Schema.GetPossibleTypes(unionDefinition).ToList(),
+            InterfaceDefinition interfaceDefinition => Schema.GetPossibleTypes(interfaceDefinition).ToList(),
+            _ => throw new InvalidOperationException(
+                $"Cannot resolve actual type of value of abstract type {abstractType.Name}. " +
+                "Use the context.ResolveAbstractType function to map from value to actual schema type")
+        };
+
+        if (possibleTypes.Contains(typeDefinition)) return;
+        
+        throw new InvalidOperationException(
+            $"Cannot resolve actual type of value of abstract type {abstractType.Name}. " +
+            "Use the context.ResolveAbstractType function to map from value to actual schema type");
     }
 }
